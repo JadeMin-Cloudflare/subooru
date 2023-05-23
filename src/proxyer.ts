40a1b7c58a992867956c4f4bc70127e0a873c3ad
JadeMin <alt0000@klartnet.ml>
2023-05-23 21:40:22 +0900

    주석 수정

--- src/proxyer.ts
+++ src/proxyer.ts
@@ -23,24 +23,15 @@ export class Proxyer {
 
 
 		if(pathname.startsWith("/home")) {
-			/**
-			 * @description
-			 * 메인화면으로 접속 시 5분간 캐시 유지
-			 */
+			// 메인화면으로 접속 시 5분간 캐시 유지
 			return fetchWithEdgeCaching(300);
 		}
 		if(pathname.startsWith("/post/list")) {
-			/**
-			 * @description
-			 * 이미지 목록으로 접속 시 5분간 캐시 유지
-			 */
+			// 이미지 목록으로 접속 시 5분간 캐시 유지
 			return fetchWithEdgeCaching(300);
 		}
 		if(pathname.startsWith("/post/view")) {
-			/**
-			 * @description
-			 * 이미지 뷰어로 접속 시 1년간 캐시 유지
-			 */
+			// 이미지 뷰어로 접속 시 1년간 캐시 유지
 			return fetchWithEdgeCaching(31536000);
 		}
 		// 이외의 모든 요청은 7일간 캐시 유지
