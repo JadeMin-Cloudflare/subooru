cc489ed99a22c3baf61f98d723fad1940bf8f20f
JadeMin <alt0000@klartnet.ml>
2023-07-04 01:09:59 +0900

    refactor: i  refactored something

--- src/proxyer.ts
+++ src/proxyer.ts
@@ -1,4 +1,5 @@
-import joinUrl from "url-join";
+import joinUrl from 'url-join';
+import NotFound from "./404.tsx";
 
 
 
@@ -23,18 +24,31 @@ export class Proxyer {
 
 
 		if(
-			pathname.startsWith("/home") ||
-			pathname.startsWith("/post/list") ||
-			pathname.startsWith("/tags")
+			pathname === '/' ||
+			pathname.startsWith("/post/list")
 		) {
 			// 캐시 유지하지 않기 (로딩 속도는 브라우저 캐시에 의존)
 			return fetchWithEdgeCaching(0);
 		}
-		if(pathname.startsWith("/post/view")) {
+
+		if(
+			pathname.startsWith("/home") ||
+			pathname.startsWith("/post/view") ||
+			
+			pathname.startsWith("/ext") ||
+			pathname.startsWith("/data") ||
+			pathname.startsWith("/_images")
+		) {
 			// 이미지 뷰어로 접속 시 1년간 캐시 유지
 			return fetchWithEdgeCaching(31536000);
 		}
-		// 이외의 모든 요청은 7일간 캐시 유지
-		return fetchWithEdgeCaching(604800);
+		
+		// 이외의 모든 요청은 비활성화
+		return new Response(NotFound, {
+			status: 404,
+			headers: {
+				"content-type": "text/html;charset=UTF-8"
+			}
+		});
 	};
 };
