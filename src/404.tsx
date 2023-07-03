c8087b4666fc8b8a95f4c2c02b440bdd62a1685a
JadeMin <alt0000@klartnet.ml>
2023-07-04 01:10:49 +0900

    feat: add page `404` using Preact

---
+++ src/404.tsx
@@ -0,0 +1,11 @@
+import { h, Fragment } from 'preact';
+import { renderToString } from 'preact-render-to-string';
+
+
+
+export default renderToString(
+	<>
+		<h1>404</h1>
+		<p>해당 페이지 및 기능은 현재 비활성화되어 있습니다.</p>
+	</>
+);
