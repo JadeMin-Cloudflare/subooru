c1f1324e5b7aa77e2254d41a790d912303962bda
JadeMin <36400787+JadeMin@users.noreply.github.com>
2023-07-30 23:57:34 +0900

    refactor(404.preact): add newline

--- src/404.tsx
+++ src/404.tsx
@@ -17,7 +17,10 @@ export default function NotFound() {
 				<h1>404</h1>
 				<p>해당 페이지 및 기능은 현재 비활성화되어 있습니다.</p>
 				<footer>
-					{currentTime.toFormat("yyyy-MM-dd a h:m")}
+					<p>
+						서버 시간<br/>
+						{currentTime.toFormat("yyyy-MM-dd a h:m")}
+					</p>
 				</footer>
 			</body>
 		</html>
