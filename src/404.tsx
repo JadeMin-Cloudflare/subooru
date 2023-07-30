2ca115903398c4f22ecfac3aae424d7506538885
JadeMin <36400787+JadeMin@users.noreply.github.com>
2023-07-31 00:11:25 +0900

    refactor(404.preact): refactor debug table

--- src/404.tsx
+++ src/404.tsx
@@ -18,10 +18,12 @@ export default function NotFound() {
 				<p>해당 페이지 및 기능은 현재 비활성화되어 있습니다.</p>
 				<details>
 					<summary>Server Debug</summary>
-					<table style={{
-						marginLeft: "auto",
-						marginRight: "auto",
-					}}>
+					<table
+						style={{
+							marginLeft: "auto",
+							marginRight: "auto",
+						}}
+					>
 						<tr>
 							<th scope="col">Date/Time</th>
 						</tr>
